6661b92505375d3624127a7c32ea1ed99e295cf5
HenriqueDeBritto <pradohenrique162@gmail.com>
2025-05-22 14:51:25 -0300

    primeiro comit

---
+++ src/test/java/br/com/henriqueprado/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.henriqueprado.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
